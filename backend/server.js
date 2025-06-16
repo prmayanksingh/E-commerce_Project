292ad2da158f86fb0e30eef8feeccd42b98e2030
prmayanksingh <prmayanksingh05@gmail.com>
2025-06-16 14:18:00 +0530

    Completed backend setup and user registration API

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes")
 
 // Load environment variables from .env
 dotenv.config();
@@ -14,6 +15,8 @@ const app = express();
 // Allows app to accept JSON data in requests (like when you send data from a form or frontend using POST)
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+
 // Test route
 app.get("/", (req, res) => {
   res.send("API is running...");
@@ -21,6 +24,7 @@ app.get("/", (req, res) => {
 
 // Sets the port on which server runs
 const PORT = process.env.PORT || 5000;
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
